da99a112e9e700af392e2144e68c305afbe9be8c
spiritphyz <tonyle@ocf.berkeley.edu>
2016-10-17 15:12:09 -0700

    Static file serving is working

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ var express = require('express');
 // attach services
 var app = express();
 
+// register location of static files
+app.use(express.static(__dirname + '/client'));
+
+// default route
+app.get('/', function(req, res) {
+  res.sendFile('./index.html', {'root': '.'});
+});
+
+// wildcard for other routes
+app.get('*', function(req, res) {
+  res.status(404).end('Not Found');
+});
+
 // start the server
 app.listen(8080);
 console.log('🍊  Listening on port 8080');
