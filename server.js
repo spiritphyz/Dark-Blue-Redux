e66a8e57b20852e568737be5bb47151d24d69ca3
spiritphyz <tonyle@ocf.berkeley.edu>
2016-10-17 17:23:16 -0700

    Set up bodyParser for client data

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 // attach services
 var app = express();
 
 // register location of static files
 app.use(express.static(__dirname + '/client'));
+app.use(bodyParser.json());
 
 // default route
 app.get('/', function(req, res) {
